d78b1433beb4cc62ece9b5771a26dc5f5245ad65
Yifan Yu <yvifan@gmail.com>
2018-03-22 19:18:32 +0200

    gen.lua is a simplified version of generate.lua

---
+++ gen.lua
@@ -0,0 +1,31 @@
+require 'image'
+require 'nn'
+torch.setdefaulttensortype('torch.FloatTensor')
+
+--noise = torch.Tensor(64, 100, 1, 1)
+noise = torch.zeros(64, 100, 1, 1)
+net = torch.load('bedrooms_4_net_G.t7')
+
+-- for older models, there was nn.View on the top
+-- which is unnecessary, and hinders convolutional generations.
+if torch.type(net:get(1)) == 'nn.View' then
+    net:remove(1)
+    print(net)
+end
+
+
+-- noise:normal(0, 1)
+-- net:float() -- what does this do?
+
+local q = require 'quantize'
+net:quantize(q.fixed(1, 8))
+
+-- 64 x 3 x 64 x 64
+local images = net:forward(noise)
+print(net.modules[1].output:size())
+print(net.modules[1].weight:size())
+-- net, net.modules, and net.modules[1] are all tables, but
+-- net.modules[1].weight and net.modules[1].output are userdata which
+-- cannot be inspected directly
+images:add(1):mul(0.5)
+image.save('gen.png', image.toDisplayTensor(images))
