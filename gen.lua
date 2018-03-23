d36b92ad79be70fe1e2767fc07158f55be8c6f9b
Yifan Yu <yvifan@gmail.com>
2018-03-23 08:31:27 +0200

    Use random noise as the input in gen.lua

--- gen.lua
+++ gen.lua
@@ -2,8 +2,8 @@ require 'image'
 require 'nn'
 torch.setdefaulttensortype('torch.FloatTensor')
 
---noise = torch.Tensor(64, 100, 1, 1)
-noise = torch.zeros(64, 100, 1, 1)
+noise = torch.Tensor(64, 100, 1, 1)
+--noise = torch.zeros(64, 100, 1, 1)
 net = torch.load('bedrooms_4_net_G.t7')
 
 -- for older models, there was nn.View on the top
@@ -13,8 +13,7 @@ if torch.type(net:get(1)) == 'nn.View' then
     print(net)
 end
 
-
--- noise:normal(0, 1)
+noise:normal(0, 1)
 -- net:float() -- what does this do?
 
 local q = require 'quantize'
@@ -22,10 +21,9 @@ net:quantize(q.fixed(1, 8))
 
 -- 64 x 3 x 64 x 64
 local images = net:forward(noise)
-print(net.modules[1].output:size())
-print(net.modules[1].weight:size())
+
 -- net, net.modules, and net.modules[1] are all tables, but
--- net.modules[1].weight and net.modules[1].output are userdata which
--- cannot be inspected directly
-images:add(1):mul(0.5)
+-- net.modules[1].weight and net.modules[1].output are userdata which cannot be inspected directly
+
+-- images:add(1):mul(0.5) -- what does this do? compare generated images with/without this
 image.save('gen.png', image.toDisplayTensor(images))
