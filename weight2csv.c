e915fa815602212043e7293dd3eeb88e062114de
Yifan Yu <yvifan@gmail.com>
2018-03-30 15:24:19 +0300

    weight2csv will convert any binary file of floats into csv

--- weight2csv.c
+++ weight2csv.c
@@ -1,36 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main()
+int main(int argc, char **argv)
 {
-    int nInputPlane = 100;
-    int nOutputPlane = 512;
-    int kW = 4;
-    int kH = 4;
-
-    long weight_size = nInputPlane * nOutputPlane * kW * kH;
-    float *weight = calloc(weight_size, sizeof(float));
-    float *bias = calloc(nOutputPlane, sizeof(float));
-
-    FILE *fp = fopen("bin/weight_1.bin", "rb");
-    fread(weight, sizeof(float), weight_size, fp);
-    fclose(fp);
-    fp = fopen("bin/bias_1.bin", "rb");
-    fread(bias, sizeof(float), nOutputPlane, fp);
+    FILE *fp = fopen(argv[1], "rb");
+    fseek(fp, 0, SEEK_END);
+    long size = ftell(fp);
+    long n_float = size / sizeof(float);
+    float *data = calloc(size, sizeof(float));
+    rewind(fp);
+    fread(data, sizeof(float), n_float, fp);
     fclose(fp);
 
-    fp = fopen("csv/weight_1.csv", "w");
-    for (int i = 0; i < weight_size; i++)
-        fprintf(fp, "%f\n", weight[i]);
+    char csv_path[256];
+    strcpy(csv_path, argv[1]);
+    for (char *p = csv_path + strlen(csv_path) - 1; p != csv_path; p--) {
+        if (*p == '.') {
+            strcpy(p, ".csv");
+            break;
+        }
+    }
+    printf("csv_path: %s\n", csv_path);
+
+    fp = fopen(csv_path, "w");
+    for (int i = 0; i < n_float; i++)
+        fprintf(fp, "%f\n", data[i]);
     fclose(fp);
 
-    fp = fopen("csv/bias_1.csv", "w");
-    for (int i = 0; i < nOutputPlane; i++)
-        fprintf(fp, "%f\n", bias[i]);
-    fclose(fp);
-
-    free(weight);
-    free(bias);
+    free(data);
     return 0;
 }
 
