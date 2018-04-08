f58210141d3ee6602883cf9f082c345fa27ba105
Yifan Yu <yvifan@gmail.com>
2018-04-08 20:27:24 +0300

    Add sparse_gemm_fixed

--- fixed/gemm.h
+++ fixed/gemm.h
@@ -23,4 +23,16 @@ void gemm_fixed(
           uint8_t beta, int32_t *c, long ldc,
           struct Q *lhs, struct Q *rhs, struct Q *res);
 
+void sparse_gemm_fixed(long m, long n, long k,
+                       uint8_t *a, long lda, uint8_t *b, long ldb,
+                       long nOutputPlane,
+                       long outputHeight, long outputWidth,
+                       long inputHeight, long inputWidth,
+                       int kH, int kW,
+                       int padH, int padW,
+                       int strideH, int strideW,
+                       int dilationH, int dilationW,
+                       int32_t* data_im,
+                       struct Q *lhs, struct Q *rhs, struct Q *res);
+
 #endif
